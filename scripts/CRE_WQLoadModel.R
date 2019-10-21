6f3a0f966abc03bb163518a33bff52859a0b93fb
SwampThingPaul <pjulian@ufl.edu>
2019-10-21 15:45:49 -0400

    additions

--- scripts/CRE_WQLoadModel.R
+++ scripts/CRE_WQLoadModel.R
@@ -21,6 +21,7 @@ library(mblm)
 library(rkt)
 library(car)
 library(pedometrics)
+library(MASS)
 
 #Paths
 wd="D:/Work/LOSOM_WQ"
@@ -183,7 +184,7 @@ plot(mean.TN~format(wq.dat.xtab.mon$monCY,"%m"),wq.dat.xtab.mon)
 plot(mean.TP~format(wq.dat.xtab.mon$monCY,"%m"),wq.dat.xtab.mon)
 
 cre.hydro.wq.mon=merge(cre.hydro.mon,wq.dat.xtab.mon,c("WY","monCY"))
-cre.hydro.wq.mon$hydro.seaon=FL.Hydroseason(cre.hydro.wq.mon$monCY)
+cre.hydro.wq.mon$hydro.season=FL.Hydroseason(cre.hydro.wq.mon$monCY)
 cre.hydro.wq.mon$month=as.numeric(format(cre.hydro.wq.mon$monCY,"%m"))
 cre.hydro.wq.mon=merge(cre.hydro.wq.mon,data.frame(month=c(5:12,1:4),month.plot=1:12),"month",all.x=T)
 cre.hydro.wq.mon=cre.hydro.wq.mon[order(cre.hydro.wq.mon$monCY),]
@@ -234,6 +235,11 @@ dev.off()
 wq.dat.xtab.mon$decWY=decimal.WY(wq.dat.xtab.mon$monCY)
 wq.dat.xtab.mon$month=as.numeric(format(wq.dat.xtab.mon$monCY,"%m"))
 wq.dat.xtab.mon$CY=as.numeric(format(wq.dat.xtab.mon$monCY,"%Y"))
+
+WYs=WY(dates[1]):WY(dates[2])
+months=rep(1:12,length(WYs))
+wq.dat.xtab.mon2=merge(data.frame(WY=sort(rep(WYs,12)),month=months,fill=1),wq.dat.xtab.mon,c("WY","month"),all.x=T)
+
 #TP
 #Autocorrelation
 acf(wq.dat.xtab.mon$mean.TP)
@@ -242,6 +248,9 @@ pacf(wq.dat.xtab.mon$mean.TP)
 cor.test(wq.dat.xtab.mon$mean.TP[-nrow(wq.dat.xtab.mon)],wq.dat.xtab.mon$mean.TP[-1],method="spearman")
 #lag 2
 cor.test(wq.dat.xtab.mon$mean.TP[-((nrow(wq.dat.xtab.mon)-1):nrow(wq.dat.xtab.mon))],wq.dat.xtab.mon$mean.TP[-(1:2)],method="spearman")
+#lag 3
+cor.test(wq.dat.xtab.mon$mean.TP[-((nrow(wq.dat.xtab.mon)-2):nrow(wq.dat.xtab.mon))],wq.dat.xtab.mon$mean.TP[-(1:3)],method="spearman")
+cor.test(wq.dat.xtab.mon$mean.TP[-((nrow(wq.dat.xtab.mon)-10):nrow(wq.dat.xtab.mon))],wq.dat.xtab.mon$mean.TP[-(1:11)],method="spearman")
 acf(wq.dat.xtab.mon$mean.TP,plot=F)
 acf(wq.dat.xtab.mon$mean.TP)
 
@@ -253,13 +262,8 @@ plot(mean.TP~decWY,wq.dat.xtab.mon,las=1,type="b")
 abline(with(subset(wq.dat.xtab.mon,is.na(mean.TP)==F),mblm(mean.TP~decWY,repeated = T)),col="red")
 
 #seasonal kendall
-with(wq.dat.xtab.mon,rkt(decimal_date(monCY),mean.TP,month))
-#par(family="serif",mar=c(1,1,1,1),oma=c(0.5,0.5,0.5,0.5));
-#layout(matrix(1:12,3,4))
-#for(i in 1:12){
-#  hist(subset(wq.dat.xtab.mon,month==i)$mean.TP,xlim=c(0.06,0.3))
-#  mtext(side=3,month.name[i])
-#}
+wq.dat.xtab.mon$hydro.season=FL.Hydroseason(wq.dat.xtab.mon$monCY)
+with(subset(wq.dat.xtab.mon,is.na(mean.TP)==F),rkt(decimal_date(monCY),mean.TP,month))
 
 #TN
 #Autocorrelation
@@ -279,7 +283,7 @@ plot(mean.TN~decWY,wq.dat.xtab.mon,las=1,type="b")
 abline(with(subset(wq.dat.xtab.mon,is.na(mean.TN)==F),mblm(mean.TN~decWY,repeated = T)),col="red")
 
 #seasonal kendall
-with(wq.dat.xtab.mon,rkt(decimal_date(monCY),mean.TN,month))
+with(subset(wq.dat.xtab.mon,is.na(mean.TN)==F),rkt(decimal_date(monCY),mean.TN,month))
 
 # Annual
 wq.dat.xtab.WY=ddply(subset(wq.dat.xtab,Station.ID=="S79"),c("WY"),summarise,mean.TP=mean(TP,na.rm=T),N.TP=N(TP),mean.TN=mean(TN,na.rm=T),N.TN=N(TN))
@@ -294,5 +298,166 @@ plot(mean.TN~WY,wq.dat.xtab.WY,las=1,type="b")
 abline(with(subset(wq.dat.xtab.WY,is.na(mean.TN)==F&N.TP>=4),mblm(mean.TN~WY,repeated = T)),col="Red")
 
 
+#tiff(filename=paste0(plot.path,"S79_MonthlyTPTN_TS.tiff"),width=5,height=6,units="in",res=200,type="windows",compression=c("lzw"),bg="white")
+par(family="serif",mar=c(1,3,0.75,0.75),oma=c(2,1,0.5,0.5));
+layout(matrix(1:2,2,1))
+ylim.val=c(0.05,0.4);ymaj=c(0.05,log.scale.fun(ylim.val,"major"),0.4);ymin=log.scale.fun(ylim.val,"minor")
+xlim.val=c(1999,2020);xmaj=seq(xlim.val[1],xlim.val[2],5);xmin=seq(xlim.val[1],xlim.val[2],1)
+plot(mean.TP~decWY,wq.dat.xtab.mon,log="y",ylim=ylim.val,xlim=xlim.val,type="n",yaxs="i",axes=F,ylab=NA,xlab=NA)
+abline(h=ymaj,v=xmaj,lty=3,col="grey")
+with(wq.dat.xtab.mon,pt_line(decWY,mean.TP,2,adjustcolor("dodgerblue1",0.5),1,21,adjustcolor("dodgerblue1",0.5),cex=1))
+mblm.mod=with(subset(wq.dat.xtab.mon,is.na(mean.TP)==F),mblm(mean.TP~decWY,repeated = T))
+mod.pred=predict(mblm.mod,data.frame(decWY=seq(1999,2020,1)),interval="confidence")
+shaded.range(seq(1999,2020,1),mod.pred[,2],mod.pred[,3],"black",lty=0)
+lines(seq(1999,2020,1),mod.pred[,1],lty=2)
+#axis_fun(1,line=-0.5,xmaj,xmin,paste(5,xmaj-1,sep=" - "))
+axis_fun(1,line=-0.5,xmaj,xmin,NA)
+axis_fun(2,ymaj,ymin,format(ymaj*1000));box(lwd=1)
+mtext(side=3,"S-79")
+mtext(side=2,line=2.5,"Total Phosphorus (\u03BCg L\u207B\u00B9)")
+legend("topright",c("Monthly Mean","Thiel-Sen \u00B1 95% CI"),
+       pch=c(NA,22),
+       lty=c(2,2),lwd=c(0.01,1),
+       col=c("dodgerblue1","grey"),
+       pt.bg=c(adjustcolor("dodgerblue1",0.25),"grey"),
+       pt.cex=1,ncol=1,cex=0.8,bty="n",y.intersp=1,x.intersp=0.75,xpd=NA,xjust=0.5,yjust=0.5,text.col="white")
+legend("topright",c("Monthly Mean","Thiel-Sen \u00B1 95% CI"),
+       pch=c(21,NA),
+       lty=c(NA,2),lwd=c(0.01,1),
+       col=c("black","black"),
+       pt.bg=c(adjustcolor("dodgerblue1",0.5),"grey"),
+       pt.cex=1,ncol=1,cex=0.8,bty="n",y.intersp=1,x.intersp=0.75,xpd=NA,xjust=0.5,yjust=0.5)
+
+ylim.val=c(0.9,3);by.y=0.5;ymaj=seq(max(1,ylim.val[1]),ylim.val[2],by.y);ymin=seq(max(1,ylim.val[1]),ylim.val[2],by.y/2)#log.scale.fun(ylim.val,"major");ymin=log.scale.fun(ylim.val,"minor")
+plot(mean.TN~decWY,wq.dat.xtab.mon,ylim=ylim.val,xlim=xlim.val,type="n",yaxs="i",axes=F,ylab=NA,xlab=NA)
+abline(h=ymaj,v=xmaj,lty=3,col="grey")
+with(wq.dat.xtab.mon,pt_line(decWY,mean.TN,2,adjustcolor("dodgerblue1",0.5),1,21,adjustcolor("dodgerblue1",0.5),cex=1))
+mblm.mod=with(subset(wq.dat.xtab.mon,is.na(mean.TN)==F),mblm(mean.TN~decWY,repeated = T))
+mod.pred=predict(mblm.mod,data.frame(decWY=seq(1999,2020,1)),interval="confidence")
+shaded.range(seq(1999,2020,1),mod.pred[,2],mod.pred[,3],"black",lty=0)
+lines(seq(1999,2020,1),mod.pred[,1],lty=2)
+axis_fun(1,line=-0.5,xmaj,xmin,paste(5,xmaj-1,sep="/"))
+axis_fun(2,ymaj,ymin,format(ymaj));box(lwd=1)
+mtext(side=1,line=1.75,"Date (Month - Year)")
+mtext(side=2,line=2.5,"Total Nitrogen (mg L\u207B\u00B9)")
+dev.off()
+
+#tiff(filename=paste0(plot.path,"S79_MonthlyTP_hist.tiff"),width=3,height=6.5,units="in",res=200,type="windows",compression=c("lzw"),bg="white")
+par(family="serif",mar=c(0.25,3,0.75,2),oma=c(4,1,0.5,0.5));
+layout(matrix(1:12,12,1))
+
+xlim.val=c(0.06,0.35);by.x=0.05;xmaj=seq(min(0,xlim.val[1]),xlim.val[2],by.x);xmin=seq(min(0,xlim.val[1]),xlim.val[2],by.x/2)
+ylim.val=c(0,6);by.y=3;ymaj=seq(ylim.val[1],ylim.val[2],by.y);ymin=seq(ylim.val[1],ylim.val[2],by.y/2)
+for(i in 1:12){
+  tmp.dat=subset(wq.dat.xtab.mon,month==i)$mean.TP
+  w=0.01;#width
+  startcat <- floor(min(tmp.dat,na.rm=TRUE)/w)*w
+  breaks.val <- seq(startcat,max(tmp.dat,na.rm=TRUE)+w,w)
+  hist(tmp.dat,main=NA,xlim=xlim.val,ylim=ylim.val,breaks=breaks.val,xlab=NA,ylab=NA,col="skyblue3",yaxs="i",axes=F)
+  if(i==12){axis_fun(1,line=-0.5,xmaj,xmin,format(xmaj))}else(axis_fun(1,xmaj,xmin,NA))
+  axis_fun(2,ymaj,ymin,ymaj)
+  box(lwd=1)
+  mtext(month.abb[i],las=3,side=4)
+  if(i==12){mtext(side=1,line=2.5,"Total Phosphorus (\u03BCg L\u207B\u00B9)")}
+}
+mtext(side=2,line=-0.5,outer=T,"Frequency")
+dev.off()
+
+#tiff(filename=paste0(plot.path,"S79_MonthlyTPWY.tiff"),width=3,height=6.5,units="in",res=200,type="windows",compression=c("lzw"),bg="white")
+par(family="serif",mar=c(0.25,3.5,0.75,1.75),oma=c(4,1,0.5,0.5));
+layout(matrix(1:12,12,1))
+
+xlim.val=c(1999,2019);by.x=5;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/by.x)
+ylim.val=c(0,0.35);by.y=0.15;ymaj=seq(ylim.val[1],ylim.val[2],by.y);ymin=seq(ylim.val[1],ylim.val[2],by.y/2)
+for(i in 1:12){
+  tmp.dat=subset(wq.dat.xtab.mon2,month==i)
+  plot(mean.TP~WY,tmp.dat,ylim=ylim.val,xlim=xlim.val,type="n",yaxs="i",axes=F,ylab=NA,xlab=NA)
+  abline(h=ymaj,v=xmaj,lty=3,col="grey")
+  #with(tmp.dat,points(WY,mean.TP,pch=21,bg="skyblue2",lwd=0.1,cex=1.25))
+  with(tmp.dat,pt_line(WY,mean.TP,1,"skyblue2",1.5,21,"skyblue2",pt.lwd=0.1,cex=1.1))
+  if(i==12){axis_fun(1,line=-0.5,xmaj,xmin,format(xmaj))}else(axis_fun(1,xmaj,xmin,NA))
+  axis_fun(2,ymaj,ymin,format(ymaj*1000))
+  box(lwd=1)
+  mtext(month.abb[i],las=3,side=4,cex=0.9,line=0.25)
+  if(i==12){mtext(side=1,line=2.5,"Water Year")}
+}
+mtext(side=2,line=-0.5,outer=T,"Total Phosphorus (\u03BCg L\u207B\u00B9)")
+dev.off()
+
+#tiff(filename=paste0(plot.path,"S79_MonthlyTNWY.tiff"),width=3,height=6.5,units="in",res=200,type="windows",compression=c("lzw"),bg="white")
+par(family="serif",mar=c(0.25,3.5,0.75,1.75),oma=c(4,1,0.5,0.5));
+layout(matrix(1:12,12,1))
+
+xlim.val=c(1999,2019);by.x=5;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/by.x)
+ylim.val=c(0,3);by.y=1.5;ymaj=seq(ylim.val[1],ylim.val[2],by.y);ymin=seq(ylim.val[1],ylim.val[2],by.y/2)
+for(i in 1:12){
+  tmp.dat=subset(wq.dat.xtab.mon2,month==i)
+  plot(mean.TN~WY,tmp.dat,ylim=ylim.val,xlim=xlim.val,type="n",yaxs="i",axes=F,ylab=NA,xlab=NA)
+  abline(h=ymaj,v=xmaj,lty=3,col="grey")
+  #with(tmp.dat,points(WY,mean.TP,pch=21,bg="skyblue2",lwd=0.1,cex=1.25))
+  with(tmp.dat,pt_line(WY,mean.TN,1,"skyblue2",1.5,21,"skyblue2",pt.lwd=0.1,cex=1.1))
+  if(i==12){axis_fun(1,line=-0.5,xmaj,xmin,format(xmaj))}else(axis_fun(1,xmaj,xmin,NA))
+  axis_fun(2,ymaj,ymin,format(ymaj))
+  box(lwd=1)
+  mtext(month.abb[i],las=3,side=4,cex=0.9,line=0.25)
+  if(i==12){mtext(side=1,line=2.5,"Water Year")}
+}
+mtext(side=2,line=-0.5,outer=T,"Total Nitrogen (mg L\u207B\u00B9)")
+dev.off()
+
+
+
 ###
 ## TP Model
+#vars=c("mean.TP","grad","C43Basin","S78","S79","S77","basin.q.ratio","hydro.season")
+vars=c("mean.TP","grad","C43Basin","S77","basin.q.ratio","hydro.season")
+S79.TP.mod=lm(log(mean.TP)~.,na.omit(cre.hydro.wq.mon[,vars]))
+layout(matrix(1:4,2,2));plot(S79.TP.mod)
+shapiro.test(residuals(S79.TP.mod));hist(residuals(S79.TP.mod))
+vif(S79.TP.mod)
+summary(S79.TP.mod)
+
+#AIC model
+S79.TP.mod.sw=stepAIC(S79.TP.mod,direction="both",trace=F)
+S79.TP.mod.sw$anova
+
+par(family="serif",mar=c(0.25,3.5,0.75,1.75),oma=c(4,1,0.5,0.5));
+layout(matrix(1:6,3,2))
+ylim.val=c(0.05,0.35);ymaj=log.scale.fun(ylim.val,"major");ymin=log.scale.fun(ylim.val,"minor")
+xlim.val=c(5.5,8.6);by.x=1;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/2)
+
+plot(mean.TP~grad,cre.hydro.wq.mon,log="y",ylim=ylim.val,xlim=xlim.val,type="n",axes=F,ylab=NA,xlab=NA)
+abline(h=ymaj,v=xmaj,lty=3,col="grey")
+with(cre.hydro.wq.mon,points(grad,mean.TP,pch=21,bg="indianred1",lwd=0.1))
+axis_fun(1,xmaj,xmin,xmaj)
+axis_fun(2,ymaj,ymin,ymaj*1000);box(lwd=1)
+mtext(side=1,line=2,"Surface Water Gradient (S235TW - S79HW; Ft NGVD29)")
+
+xlim.val=c(0,25e4);by.x=5e4;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/2)
+plot(mean.TP~C43Basin,cre.hydro.wq.mon,log="y",ylim=ylim.val,xlim=xlim.val,type="n",axes=F,ylab=NA,xlab=NA)
+abline(h=ymaj,v=xmaj,lty=3,col="grey")
+with(cre.hydro.wq.mon,points(C43Basin,mean.TP,pch=21,bg="indianred1",lwd=0.1))
+axis_fun(1,xmaj,xmin,xmaj/1e4)
+axis_fun(2,ymaj,ymin,ymaj*1000);box(lwd=1)
+mtext(side=1,line=2,expression(paste("Q"["C43Basin"]," x10"^3,"; CFS")))
+
+xlim.val=c(0,20e4);by.x=5e4;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/2)
+plot(mean.TP~S77,cre.hydro.wq.mon,log="y",ylim=ylim.val,xlim=xlim.val,type="n",axes=F,ylab=NA,xlab=NA)
+abline(h=ymaj,v=xmaj,lty=3,col="grey")
+with(cre.hydro.wq.mon,points(S77,mean.TP,pch=21,bg="indianred1",lwd=0.1))
+axis_fun(1,xmaj,xmin,xmaj/1e4)
+axis_fun(2,ymaj,ymin,ymaj*1000);box(lwd=1)
+mtext(side=1,line=2,expression(paste("Q"["S77"]," x10"^3,"; CFS")))
+
+xlim.val=c(0,1);by.x=0.2;xmaj=seq(xlim.val[1],xlim.val[2],by.x);xmin=seq(xlim.val[1],xlim.val[2],by.x/2)
+plot(mean.TP~basin.q.ratio,cre.hydro.wq.mon,log="y",ylim=ylim.val,xlim=xlim.val,type="n",axes=F,ylab=NA,xlab=NA)
+abline(h=ymaj,v=xmaj,lty=3,col="grey")
+with(cre.hydro.wq.mon,points(basin.q.ratio,mean.TP,pch=21,bg="indianred1",lwd=0.1))
+axis_fun(1,xmaj,xmin,format(xmaj))
+axis_fun(2,ymaj,ymin,ymaj*1000);box(lwd=1)
+mtext(side=1,line=2,expression(paste("Q"["C43Basin"],": Q"["S79"])))
+
+tmp=ddply(cre.hydro.wq.mon,"hydro.season",summarise,mean.val=mean(mean.TP,na.rm=T),SE.val=SE(mean.TP))
+
+ylim.val=c(0,0.2);
+barplot(tmp$mean.val,ylim=ylim.val)
